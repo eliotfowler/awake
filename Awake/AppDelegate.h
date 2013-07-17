97dab2b7699a9a2f5a8204cc14879b948c3da122
Eliot Fowler <eliot@CSGCINMAC04.local>
2013-07-17 09:38:34 -0400

    Initial Commit

---
+++ Awake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Awake
+//
+//  Created by Eliot Fowler on 7/17/13.
+//  Copyright (c) 2013 Eliot Fowler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AwakeTests/AwakeTests.m
@@ -0,0 +1,36 @@
+//
+//  AwakeTests.m
+//  AwakeTests
+//
+//  Created by Eliot Fowler on 7/17/13.
+//  Copyright (c) 2013 Eliot Fowler. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AwakeTests : XCTestCase
+
+@end
+
+@implementation AwakeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
