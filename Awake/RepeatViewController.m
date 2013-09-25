f1ac1e0013b64d1a2db53097b57d5ec1db96751f
Eliot Fowler <jfowler@cardinalsolutions.com>
2013-09-25 16:58:44 -0400

    repeat string on the repeat view now accurately reflects the days

--- Awake/RepeatViewController.m
+++ Awake/RepeatViewController.m
@@ -19,6 +19,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *saturdayButton;
 @property (weak, nonatomic) IBOutlet UIButton *sundayButton;
 @property (weak, nonatomic) IBOutlet UITableView *repeatTable;
+@property (weak, nonatomic) IBOutlet UILabel *repeatLabel;
 
 
 @end
@@ -59,8 +60,9 @@
     [self setShouldRepeat:!shouldRepeatWeekday forDay:dayClicked];
     
     [self highlightButtons];
+    [self modifyRepeatLabel];
     [_repeatTable reloadData];
-    [_delegate didSetRepeatDays:_repeatDays withRepeatString:[self getRepeatString]];
+    [_delegate didSetRepeatDays:_repeatDays withRepeatString:[self getShortRepeatString]];
 }
 
 - (void) highlightButtons
@@ -76,6 +78,54 @@
         UIColor* newColor = [[button backgroundColor] colorWithAlphaComponent:alpha];
         [button setBackgroundColor:newColor];
     }
+}
+
+- (void) modifyRepeatLabel
+{
+    NSArray* weekdays = [[NSArray alloc] initWithObjects:@"monday", @"tuesday", @"wednesday", @"thursday", @"friday", @"saturday", @"sunday", nil];
+    
+    //First check if will repeat all days
+    if(_willRepeatEveryday) {
+        [_repeatLabel setText:@"Repeats every day."];
+    } else if(_willRepeatWeekdays) {
+        [_repeatLabel setText:@"Repeats on weekdays."];
+    } else if(_willRepeatWeekends) {
+        [_repeatLabel setText:@"Repeats on weekends."];
+    } else {
+        NSString *newLabelText = @"Repeats on";
+        NSMutableArray *daysAlarmWillRepeat = [[NSMutableArray alloc] init];
+        //Find all weekdays that this will repeat on
+        for(int i=0; i<[weekdays count]; i++) {
+            NSString* key = weekdays[i];
+            if([[_repeatDays objectForKey:key] boolValue]) {
+                [daysAlarmWillRepeat addObject:[weekdays objectAtIndex:i]];
+            }
+        }
+        //If there are no days that this repeats
+        if([daysAlarmWillRepeat count] == 0) {
+            newLabelText = @"Never repeats.";
+        }
+        //If it only repeats one day
+        else if([daysAlarmWillRepeat count] == 1) {
+            newLabelText = [NSString stringWithFormat:@"Repeats on %@.",[daysAlarmWillRepeat objectAtIndex:0]];
+        }
+        //If it repeats 2 days, just connect with and
+        else if([daysAlarmWillRepeat count] == 2) {
+            newLabelText = [NSString stringWithFormat:@"Repeats on %@ and %@.",[daysAlarmWillRepeat objectAtIndex:0], [daysAlarmWillRepeat objectAtIndex:1]];
+        }
+        //Otherwise, it gets tricky
+        else {
+            NSString *formatString = [daysAlarmWillRepeat objectAtIndex:0];
+            for (int i=1; i<daysAlarmWillRepeat.count-1; i++) {
+                formatString = [NSString stringWithFormat:@"%@, %@", formatString, [daysAlarmWillRepeat objectAtIndex:i]];
+            }
+            formatString = [NSString stringWithFormat:@"%@ and %@.", formatString, [daysAlarmWillRepeat objectAtIndex:[daysAlarmWillRepeat count]-1]];
+            newLabelText = [NSString stringWithFormat:@"Repeats %@", formatString];
+        }
+        
+        [_repeatLabel setText:newLabelText];
+    }
+    
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -141,7 +191,8 @@
     
     [tableView reloadData];
     [self highlightButtons];
-    [_delegate didSetRepeatDays:_repeatDays withRepeatString:[self getRepeatString]];
+    [self modifyRepeatLabel];
+    [_delegate didSetRepeatDays:_repeatDays withRepeatString:[self getShortRepeatString]];
 }
 
 #pragma repeatMethods
@@ -237,7 +288,7 @@
     _willRepeatWeekdays = NO;
 }
 
-- (NSString*) getRepeatString
+- (NSString*) getShortRepeatString
 {
     if(_willRepeatEveryday) {
         return @"Repeats everyday";
